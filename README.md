cf6b9319792ccf3f3da1cfb1c61d9979c0af44e5
fahamjv <fahamjv@gmail.com>
2023-09-27 02:00:00 +0330

    refactor plus updating README

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # Cloudflare Bulk DNS Delete
 
 This repository provides a PHP script for bulk deleting DNS records in Cloudflare using the Cloudflare API.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- PHP installed on your local machine or server.
+- Composer installed for managing dependencies.
+- A Cloudflare account with an active Zone ID.
+- Cloudflare API Token with the necessary permissions.
+
+## Installation
+
+1. Clone this repository to your local machine:
+    ```shell
+    git clone https://github.com/fahamjv/cloudflare-dns-record-deletion.git
+    ```
+
+2. Change to the project directory:
+    ```shell
+    cd cloudflare-dns-record-deletion
+    ```
+
+3. Install the required dependencies using Composer:
+    ```shell
+    composer install
+    ```
+
+
+## Usage
+
+1. Replace the placeholders in the script with your Cloudflare Zone ID and API Token:
+    ```PHP
+    $zoneId = "YOUR_ZONE_ID";
+    $bearerToken = "YOUR_API_TOKEN";
+    ```
+
+2. Run the script:
+    ```shell
+    php delete_dns_records.php
+    ```
+    The script will start deleting DNS records in batches. It will handle rate limiting and retries for failed deletions.
+
--- main.php
+++ main.php
@@ -1,64 +1,68 @@
 <?php
 
-require 'vendor/autoload.php'; // Make sure you have Guzzle installed via Composer
+require "vendor/autoload.php"; // Make sure you have Guzzle installed via Composer
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 
 /**
  * Replace with your Cloudflare Zone ID
- * 
+ *
  * @link https://dash.cloudflare.com/[your-account-id]/[domain]
  */
 $zoneId = "YOUR_ZONE_ID";
 
 /**
  * Replace with your Cloudflare API Token
- * 
+ *
  * @link https://dash.cloudflare.com/profile/api-tokens
  */
 $bearerToken = "YOUR_API_TOKEN";
 
 // Add this at the beginning of your script to log to a file
-$logFile = 'script_log.txt';
-ini_set('log_errors', 1);
-ini_set('error_log', $logFile);
+$logFile = "script_log.txt";
+ini_set("log_errors", 1);
+ini_set("error_log", $logFile);
 
 // Add this at the beginning of your script to increase memory limit and execution time
-ini_set('memory_limit', '512M');
+ini_set("memory_limit", "512M");
 set_time_limit(0);
 
 $client = new Client([
-    'base_uri' => 'https://api.cloudflare.com/client/v4/',
-    'headers' => [
-        'Content-Type' => 'application/json',
-        'Authorization' => 'Bearer ' . $bearerToken,
+    "base_uri" => "https://api.cloudflare.com/client/v4/",
+    "headers" => [
+        "Content-Type" => "application/json",
+        "Authorization" => "Bearer " . $bearerToken,
     ],
 ]);
 
 /**
  * Function to fetch DNS records from Cloudflare
- * 
+ *
  * @param   Client    $client
  * @param   string    $zoneId
  *
  * @return array
  */
-function fetchDNSRecords($client, $zoneId) {
-    $response = $client->get("zones/{$zoneId}/dns_records?type=TXT&page=1&per_page=100");
+function fetchDNSRecords($client, $zoneId)
+{
+    $response = $client->get(
+        "zones/{$zoneId}/dns_records?type=TXT&page=1&per_page=100"
+    );
     return json_decode($response->getBody(), true);
 }
 
 /**
  * Function to delete a DNS record using its ID
- * 
+ *
  * @param   Client    $client
  * @param   string    $zoneId
  * @param   string    $recordId
- * 
+ *
  * @return int|null
  */
-function deleteDNSRecord($client, $zoneId, $recordId) {
+function deleteDNSRecord($client, $zoneId, $recordId)
+{
     try {
         $response = $client->delete("zones/{$zoneId}/dns_records/{$recordId}");
         return $response->getStatusCode();
@@ -73,16 +77,16 @@ function deleteDNSRecord($client, $zoneId, $recordId) {
 $totalDeleted = 0;
 
 while (true) {
-    $dnsRecords = fetchDNSRecords($client, $zoneId)['result'];
+    $dnsRecords = fetchDNSRecords($client, $zoneId)["result"];
 
     if (empty($dnsRecords)) {
         break; // No more records to delete
     }
 
     foreach ($dnsRecords as $record) {
-        $recordId = $record['id'];
-        $recordName = $record['name'];
-        $recordType = $record['type'];
+        $recordId = $record["id"];
+        $recordName = $record["name"];
+        $recordType = $record["type"];
 
         echo "Deleting: {$recordName} [{$recordType}]\n";
 
@@ -107,4 +111,3 @@ while (true) {
 }
 
 echo "{$totalDeleted} records deleted.\n";
-
